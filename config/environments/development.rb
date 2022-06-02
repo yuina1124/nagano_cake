649ed280f6f3844fc808d787b314dce677bdc76d
EC2 Default User <ec2-user@ip-172-31-3-1.ec2.internal>
2022-06-02 09:23:46 +0000

    [Finish]diviseの導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "3684060406d440349fd8779793cc85e9.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
