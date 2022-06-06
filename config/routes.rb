fe9173539edf1d11896c5e217bd236f69290f6ac
EC2 Default User <ec2-user@ip-172-31-3-1.ec2.internal>
2022-06-06 08:57:08 +0000

    [Finish]ヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,27 @@ root to: "public/homes#top"
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: 'public' do
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete 'cart_items/destroy_all' => 'public#cart_items#destroy_all'
+    get "/customers" => "customers#show"
+    get "/customers/edit" => "customers#edit"
+    patch "/customers" => "customers#update"
+    get "/costomers/comfirmation" => "customers#comfirmation"
+    patch "/costomers" => "costomers#withdraw"
+    get "/about" => "homes#about"
+  end
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+    get "/" =>"homes#top"
+  end
 
 #顧客用
 devise_for :customers,skip: [:passwords], controllers: {
