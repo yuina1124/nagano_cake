79c5e0c4994242bf003c620684417f5d56c2c527
EC2 Default User <ec2-user@ip-172-31-3-1.ec2.internal>
2022-06-11 02:49:44 +0000

    [Update]item,show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,6 +5,10 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def new
     @item = Item.new
   end
@@ -15,10 +19,20 @@ class Admin::ItemsController < ApplicationController
     redirect_to admin_item_path(@item)
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item)
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :is_active, :genre_id)
+  end
+
+  def item_params
+    params.require(:item).permit(:image)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ApplicationRecord
   has_many :order_detail, dependent: :destroy
   has_many :cart_item, dependent: :destroy
 
+  has_one_attached :image
+
 end
